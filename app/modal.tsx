3883b3d74269dcb8cda718bd17ae3f3320553011
sebikap <sebastiankaplanski@gmail.com>
2024-09-15 04:23:36 -0300

    feat(modal): validate params

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,10 +1,16 @@
 import { useRoute } from '@react-navigation/native';
-import { useRouter } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
+import { z } from 'zod';
+
+const modalParamsValidator = z.object({
+  params: z.object({
+    title: z.string()
+  })
+});
 
 export default function Modal() {
   const route = useRoute();
-  const params = route.params;
+  const { params } = modalParamsValidator.parse(route);
   if (!params) return null;
   if (!params.title) return null;
 
