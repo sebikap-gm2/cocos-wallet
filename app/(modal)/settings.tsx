57f9cca4a4952dbdfdcf9df0668e83ee05754372
sebikap <sebastiankaplanski@gmail.com>
2024-09-22 14:43:07 -0300

    chore(settings page): minimal layout

--- app/(modal)/settings.tsx
+++ app/(modal)/settings.tsx
@@ -1,11 +1,9 @@
 import { DS } from "@/design-system";
-import { Link } from "expo-router";
 
 export default function SettingsModal() {
   return (
     <DS.PageLayout>
-      <Link href={{ pathname: '/(tabs)' }}>GO BACK</Link>
-      <DS.Text>Settings!</DS.Text>
+      <DS.Text>This would be Settings page</DS.Text>
     </DS.PageLayout>
   )
 }
