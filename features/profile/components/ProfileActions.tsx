d7f3ac51fb9f90137472190c4bc1c0241db33c22
sebikap <sebastiankaplanski@gmail.com>
2024-09-23 02:08:35 -0300

    feat(profile actions): captions

--- features/profile/components/ProfileActions.tsx
+++ features/profile/components/ProfileActions.tsx
@@ -4,9 +4,48 @@ import { Alert } from 'react-native';
 export const ProfileActions = () => {
   return (
     <DS.View row center justifyCenter gap={10}>
-      <DS.Button icon="send-outline" text="Send" onPress={() => Alert.alert('Send', 'Sendddd')} />
-      <DS.Button icon="send-outline" text="Send" onPress={() => Alert.alert('Send', 'Sendddd')} />
-      <DS.Button icon="send-outline" text="Send" onPress={() => Alert.alert('Send', 'Sendddd')} />
+      <DS.Button
+        icon="send-outline"
+        iconVertical
+        center
+        style={{ padding: 20 }}
+        type="secondary"
+        text="Send"
+        onPress={() =>
+          Alert.alert(
+            'Send',
+            'This would trigger action to send money, or maybe trigger transaction modal',
+          )
+        }
+      />
+      <DS.Button
+        icon="qr-code-outline"
+        iconVertical
+        center
+        style={{ padding: 20 }}
+        type="secondary"
+        text="Recieve"
+        onPress={() =>
+          Alert.alert(
+            'Recieve',
+            'This could send a notification to contact, send QR, or trigger transaction modal',
+          )
+        }
+      />
+      <DS.Button
+        icon="time-outline"
+        iconVertical
+        center
+        style={{ padding: 20 }}
+        type="secondary"
+        text="Activity"
+        onPress={() =>
+          Alert.alert(
+            'Activity',
+            'This could show a detailed list of the BUY and SELL actions the client took',
+          )
+        }
+      />
     </DS.View>
   );
 };
