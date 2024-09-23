7f489280034daa99aaf1d1803ec98ded94ea2520
sebikap <sebastiankaplanski@gmail.com>
2024-09-22 23:20:15 -0300

    feat(lint): add code styling rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ['expo', 'prettier'],
-  plugins: ['prettier'],
+  plugins: ['react', 'prettier'],
   rules: {
-    'prettier/prettier': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        bracketSpacing: true,
+      },
+    ],
   },
 };
