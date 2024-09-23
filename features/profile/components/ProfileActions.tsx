fb35383b9e95537d94941662f7ee778b30c63164
sebikap <sebastiankaplanski@gmail.com>
2024-09-23 01:58:12 -0300

    feat(profile): actions styling

--- features/profile/components/ProfileActions.tsx
+++ features/profile/components/ProfileActions.tsx
@@ -3,10 +3,10 @@ import { Alert } from 'react-native';
 
 export const ProfileActions = () => {
   return (
-    <>
+    <DS.View row center justifyCenter gap={10}>
       <DS.Button icon="send-outline" text="Send" onPress={() => Alert.alert('Send', 'Sendddd')} />
       <DS.Button icon="send-outline" text="Send" onPress={() => Alert.alert('Send', 'Sendddd')} />
       <DS.Button icon="send-outline" text="Send" onPress={() => Alert.alert('Send', 'Sendddd')} />
-    </>
+    </DS.View>
   );
 };
