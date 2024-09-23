62d565d499d92dba0574780d4e57abb96da7f467
sebikap <sebastiankaplanski@gmail.com>
2024-09-23 01:43:44 -0300

    feat(page layout): add loading state

--- design-system/PageLayout.tsx
+++ design-system/PageLayout.tsx
@@ -1,14 +1,23 @@
+import { Spinner } from '@/shared';
 import { PropsWithChildren } from 'react';
 import { SafeAreaView, ViewProps } from 'react-native';
+import { ThemedView } from './ThemedView';
 
-export const PageLayout: React.FC<PropsWithChildren & ViewProps> = ({
+export const PageLayout: React.FC<PropsWithChildren & ViewProps & { isLoading?: boolean }> = ({
   children,
   style,
+  isLoading,
   ...rest
 }) => {
   return (
     <SafeAreaView style={[{ flex: 1, margin: 5 }, style]} {...rest}>
-      {children}
+      {isLoading ? (
+        <ThemedView flex={1} center justifyCenter>
+          <Spinner />
+        </ThemedView>
+      ) : (
+        children
+      )}
     </SafeAreaView>
   );
 };
