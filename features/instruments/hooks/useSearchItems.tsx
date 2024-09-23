59b1a180d49e2b73acef58bc250e63346675cbbe
sebikap <sebastiankaplanski@gmail.com>
2024-09-23 02:15:33 -0300

    chore(search items): comment

--- features/instruments/hooks/useSearchItems.tsx
+++ features/instruments/hooks/useSearchItems.tsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { instrumentsService } from '../services';
 import { useDebounce } from '@/shared';
 
+// This hook purposly avoid saving data to recoil local state as it is intended to be a volatile search. Upon further detailing of the feature, this could change.
 export const useSearchItems = (text: string) => {
   const debouncedText = useDebounce(text, 300);
   const { data, isError, isLoading } = useQuery({
