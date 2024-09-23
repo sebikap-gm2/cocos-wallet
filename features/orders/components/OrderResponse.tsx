0aa3463473ffacf2d066963d4e76eb50a0379680
sebikap <sebastiankaplanski@gmail.com>
2024-09-23 01:08:10 -0300

    fix(transaction): response conditional rendering

--- features/orders/components/OrderResponse.tsx
+++ features/orders/components/OrderResponse.tsx
@@ -8,22 +8,24 @@ interface OrderResponseProps {
 }
 
 export const OrderResponse = ({ orderMutation }: OrderResponseProps) => {
+  if (orderMutation.isIdle) return null;
   return (
     <DS.View flex={1} center row gap={20}>
       {orderMutation.isPending ? (
         <Spinner />
-      ) : !orderMutation.data ? (
+      ) : orderMutation.isError ? (
         // TODO: Handle error state.
         <DS.Text>There was an error</DS.Text>
       ) : (
         <>
           <DS.View>
             <DS.Text type="defaultSemiBold">Order ID</DS.Text>
-            <DS.Text type="subtitle">{orderMutation.data.id}</DS.Text>
+            {/* TODO: Assuming if no error data is present, must investigate more. */}
+            <DS.Text type="subtitle">{orderMutation.data?.id}</DS.Text>
           </DS.View>
           <DS.View>
             <DS.Text type="defaultSemiBold">Order Status</DS.Text>
-            <DS.Text type="subtitle">{orderMutation.data.status}</DS.Text>
+            <DS.Text type="subtitle">{orderMutation.data?.status}</DS.Text>
           </DS.View>
         </>
       )}
